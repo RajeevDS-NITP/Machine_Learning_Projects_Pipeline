4895ab107eb9cc50ecc39381ed75ece1b69e2bd0
RajeevDS-NITP <rajukkm2025@gmail.com>
2025-02-19 10:52:53 +0530

    19feb work save

---
+++ fastapi-practice/auth-api.py
@@ -0,0 +1,11 @@
+"""
+TASK ::
+1. Add FastAPI Authentication (JWT, OAuth2)
+2. Add FastAPI Authorization (Role-Based Access Control)
+3. Add FastAPI API Documentation (OpenAPI, Swagger UI)
+4. Add FastAPI API Testing (Pytest, Unittest)
+5. Add FastAPI API Monitoring (Prometheus, Grafana)
+=====================================
+
+"""
+ 
